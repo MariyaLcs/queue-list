05edf0bccb74503b1d80fedd03f63eead25c81f8
Mariya <mariya.podosinova@gmail.com>
2020-07-30 13:07:37 +0100

    parse the data from the web page to the server

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-- express, body-parser
+- express, body-parser (npm i express body-parser)
+  body-parser extract the entire body portion of an incoming request stream and exposes it on req. body . The middleware was a part of Express. js earlier but now you have to install it separately. This body-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request.
 
 - EJS+express (npm i ejs)
   EJS is a simple templating language that lets you generate HTML markup with plain JavaScript.
+  app.set("view engine", "ejs");
--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   let today = new Date();
-  let currentDay = today.getDay();
+  const options = { weekday: "long", day: "numeric", month: "long" };
+  let currentDay = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: currentDay });
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  console.log(firstName + lastName);
 });
 
 app.listen(3000, function () {
