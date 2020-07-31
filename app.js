10c6407c420ae7dce57cc0bcbab34ae56440c4a8
Mariya <mariya.podosinova@gmail.com>
2020-07-31 18:34:07 +0100

    Templating + Layouts

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let fullNames = ["XXX"];
+let regularPatientNames = [];
 
 app.set("view engine", "ejs");
 
@@ -15,16 +16,43 @@ app.get("/", function (req, res) {
   const options = { weekday: "long", day: "numeric", month: "long" };
   let currentDay = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: currentDay, newPatient: fullNames });
+  res.render("list", {
+    listTitle: currentDay,
+    newPatient: fullNames,
+  });
 });
 
 app.post("/", function (req, res) {
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const fullName = `${firstName} ${lastName}`;
-  fullNames.push(fullName);
-  res.redirect("/");
+
+  if (req.body.list === "regular-patient") {
+    regularPatientNames.push(fullName);
+  } else {
+    fullNames.push(fullName);
+    res.redirect("/");
+  }
 });
+
+app.get("/regular-patient", function (req, res) {
+  res.render("list", {
+    listTitle: "Regular Patient",
+    newPatient: regularPatientNames,
+  });
+});
+
+app.post("/regular-patient", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const fullName = `${firstName} ${lastName}`;
+  regularPatientNames.push(fullName);
+  res.redirect("/regular");
+});
+
+// app.get("/firstVisit", function (req, res) {
+//   res.render("list", { listTitle: "" });
+// });
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
